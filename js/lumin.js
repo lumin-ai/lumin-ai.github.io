7e8fc1460667a745614fbc6a9891cea5a83ea7c9
Armando <flores@mtu.edu>
2016-05-12 14:53:08 -0700

    moving js to file

---
+++ js/lumin.js
@@ -0,0 +1,84 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+var openDemoForm = function() {
+  $('.demo-btn').addClass('inactive');
+  $('.demo-row').addClass('active');
+}
+var openDemoFormTwo = function() {
+  $('.demo-btn-2').addClass('inactive');
+  $('.demo-row-2').addClass('active');
+}
+$(".demo-btn").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
+  $('.demo-btn').addClass('no-display');
+  $('.demo-row').addClass('active');
+});
+$(".demo-btn-2").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
+  $('.demo-btn-2').addClass('no-display');
+  $('.demo-row-2').addClass('active');
+});
+
+$('.carousel').carousel({
+  interval: 16000
+});
+
+
+
+// Dirty form validation
+$('#formValidation').click(function() {
+  if ($('#Field1').val() == '') {
+    alert('Input can not be left blank');
+  } else if ($('#Field1').val() == '') {
+    alert('Input can not be left blank');
+  } else if ($('#Field4').val() == '') {
+    alert('Please enter a valid email address');
+  } else if ($('#Field3').val() == '') {
+    alert('Input can not be left blank');
+  } else if ($('#Field7').val() == '') {
+    alert('Input can not be left blank');
+  } else {
+    console.log("Forms filled")
+    validateForm()
+  }
+});
+
+$('#formValidation2').click(function() {
+  if ($('#Field9').val() == '') {
+    alert('Input can not be left blank 1');
+  } else if ($('#Field2').val() == '') {
+    alert('Please enter a valid email address');
+  } else if ($('#Field8').val() == '') {
+    alert('Input can not be left blank 2');
+  } else if ($('#Field6').val() == '') {
+    alert('Input can not be left blank 3');
+  } else {
+    console.log("Forms filled")
+    validateForm2()
+  }
+});
+
+
+var validateForm = function() {
+  var x = document.forms["form1"]["Field4"].value;
+  var atpos = x.indexOf("@");
+  var dotpos = x.lastIndexOf(".");
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+    alert("Please enter a valid email address");
+    return false;
+  } else {
+    document.getElementById("form1").submit();
+  }
+}
+var validateForm2 = function() {
+  var x = document.forms["form2"]["Field2"].value;
+  var atpos = x.indexOf("@");
+  var dotpos = x.lastIndexOf(".");
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+    alert("Please enter a valid email address");
+    return false;
+  } else {
+    document.getElementById("form2").submit();
+  }
+}
