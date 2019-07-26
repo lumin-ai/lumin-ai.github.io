ff172b24e2ee6c531fbfd45a08bda34c54eb5f43
yesthisiskendra <yesthisiskendra@gmail.com>
2019-07-26 15:57:29 -0700

    Addressed issues 1,2 and 4 in https://trashpandaparty.slack.com/archives/GLDPZAYKV/p1564179665029700

--- js/lumin.js
+++ js/lumin.js
@@ -1,5 +1,5 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', '/assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
